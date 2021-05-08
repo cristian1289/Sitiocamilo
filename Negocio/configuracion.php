06d919f45d43b852a4bc8b7a252cefd27c5cf445
camilo <cristian.salinas@unisarc.edu.co>
2021-05-08 17:11:30 -0500

    Se inicia proceso de setear la configuarción en un archivo

--- Negocio/configuracion.php
+++ Negocio/configuracion.php
@@ -1,31 +1,31 @@
 <?php
 if (isset($_POST['txtServidor']))
 {
-    echo'se recupero el servidor: '.$post['txtServidor'].' <br />';
+    echo'se recupero el servidor: '.$_POST['txtServidor'].' <br />';
 }
 else{
-    echo 'No se recupero el servidor'
+    echo 'No se recupero el servidor';
 }
 if (isset($_POST['txtBasedeDatos']))
 {
-    echo'se recupero la Base de Datos: '.$post['txtBasedeDatos'].' <br />';
+    echo'se recupero la Base de Datos: '.$_POST['txtBasedeDatos'].' <br />';
 }
 else{
-    echo 'No se recupero la Base de Datos'
+    echo 'No se recupero la Base de Datos';
 }
 if (isset($_POST['txtUsuario']))
 {
-    echo'se recupero el Usuario: '.$post['txtUsuario'].' <br />';
+    echo'se recupero el Usuario: '.$_POST['txtUsuario'].' <br />';
 }
 else{
-    echo 'No se recupero el Usuario'
+    echo 'No se recupero el Usuario';
 }
 if (isset($_POST['txtContraseña']))
 {
-    echo'se recupero la contraseña: '.$post['txtContraseña'].' <br />';
+    echo'se recupero la contraseña: '.$_POST['txtContraseña'].' <br />';
 }
 else{
-    echo 'No se recupero la contraseña'
+    echo 'No se recupero la contraseña';
 }
 
 
